82874e51338e46f82ad6dc6de92364d51b9b2798
navyavinitha <143788559+navyavinitha@users.noreply.github.com>
2026-04-09 11:57:52 +0530

    Add files via upload

---
+++ Remove_Duplicates.py
@@ -0,0 +1,11 @@
+def remove_duplicates(s):
+    result = ""
+
+    for ch in s:
+        if ch not in result:
+            result += ch
+
+    return result
+print(remove_duplicates("programming"))
+print(remove_duplicates("hello"))
+print(remove_duplicates("aabbcc"))   
